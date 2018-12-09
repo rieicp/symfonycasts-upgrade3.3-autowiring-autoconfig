86f3147fcd9d6fa6402e78ef26a1b4d1550e4bc5
Ning <ning.wei@int-trade.de>
2018-12-09 13:41:38 +0100

    using MessageManager

--- src/AppBundle/EventSubscriber/AddNiceheaderSubscriber.php
+++ src/AppBundle/EventSubscriber/AddNiceheaderSubscriber.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace AppBundle\EventSubscriber;
+use AppBundle\Service\MessageManager;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
@@ -12,18 +13,25 @@ class AddNiceheaderSubscriber implements EventSubscriberInterface
      * @var LoggerInterface
      */
     private $logger;
+    /**
+     * @var MessageManager
+     */
+    private $messageManager;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, MessageManager $messageManager)
     {
         $this->logger = $logger;
+        $this->messageManager = $messageManager;
     }
 
     public function onKernelResponse(FilterResponseEvent $event)
     {
         $this->logger->info('Adding a nice header!');
 
+        $message = $this->messageManager->getEncouragingMessage();
+
         $event->getResponse()
-            ->headers->set('X-NICE-MESSAGE', 'That was a great request');
+            ->headers->set('X-NICE-MESSAGE', $message);
     }
 
     public static function getSubscribedEvents()
