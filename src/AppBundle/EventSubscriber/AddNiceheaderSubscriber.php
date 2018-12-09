a46fa1f39f83caaa9c95e604e13323979d70519f
Ning <ning.wei@int-trade.de>
2018-12-09 13:26:01 +0100

    create EventSubscriber to add response header

---
+++ src/AppBundle/EventSubscriber/AddNiceheaderSubscriber.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\EventSubscriber;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class AddNiceheaderSubscriber implements EventSubscriberInterface
+{
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        $event->getResponse()
+            ->headers->set('X-NICE-MESSAGE', 'That was a great request');
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::RESPONSE => 'onKernelResponse'
+        ];
+    }
+
+}
