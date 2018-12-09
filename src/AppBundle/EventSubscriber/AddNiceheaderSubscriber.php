62e6e3068b81482f8a5539c629774bbabd2b7372
Ning <ning.wei@int-trade.de>
2018-12-09 13:37:00 +0100

    using LoggerInterface for Type-hint

--- src/AppBundle/EventSubscriber/AddNiceheaderSubscriber.php
+++ src/AppBundle/EventSubscriber/AddNiceheaderSubscriber.php
@@ -1,14 +1,27 @@
 <?php
 
 namespace AppBundle\EventSubscriber;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class AddNiceheaderSubscriber implements EventSubscriberInterface
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function onKernelResponse(FilterResponseEvent $event)
     {
+        $this->logger->info('Adding a nice header!');
+
         $event->getResponse()
             ->headers->set('X-NICE-MESSAGE', 'That was a great request');
     }
